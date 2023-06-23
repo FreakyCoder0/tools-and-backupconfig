d0c20686ed66e5a63de7658a815c75ea3aa853fa
Harsh <polasharsh@gmail.com>
2023-06-23 10:33:07 +0530

    final commit

--- NMSapplication/tasks.py
+++ NMSapplication/tasks.py
@@ -1,6 +1,6 @@
 from celery import shared_task
 from NMSapplication.utils.backup_launcher import BackupLuancher
-# ,TopologyDiscovery
+from NMSapplication.utils.topology import Topology_Discovery
 
 
 @shared_task(name ='backup',bind = True)
@@ -9,11 +9,11 @@ def backup_launch(self,*args,**kwargs):
     backup_obj = BackupLuancher(task_id)
     backup_obj.main()
 
-# @shared_task(name = 'backup',bind = True)
-# def topology_discovery(self,*args,**kwargs):
-#     task_id = self.request.id  
-#     backup_obj = TopologyDiscovery(task_id)
-#     backup_obj.main()
+@shared_task(name = 'TopologyDiscovery',bind = True)
+def topology_discovery(self,*args,**kwargs):
+    task_id = self.request.id  
+    backup_obj = Topology_Discovery(task_id)
+    backup_obj.main()
     
     
 
--- NMSapplication/utils/topology.py
+++ NMSapplication/utils/topology.py
@@ -1,3 +1,24 @@
+from NMSapplication.models import Launchscheduler,Scheduler
+from django.core.mail import send_mail
+from NMSproject import settings
 
+class Topology_Discovery:
+    def __init__(self,task_id):
+        self.logs={}
+        self.task_id = task_id
 
-# class T
+    def main(self):
+        self.logs['data']= "Topology Discovery xjiceisvbciebc wnnxiw" 
+        created = Launchscheduler.objects.create(
+            task_id=self.task_id,
+            scheduler_type='TopologyDiscovery',
+            logs=self.logs
+        )
+        email_subject = Scheduler.objects.filter(scheduler_type='TopologyDiscovery').last()
+        send_mail( 
+                subject = email_subject.email_subject, 
+                message = self.logs['data'], 
+                from_email = settings.EMAIL_HOST_USER, 
+                recipient_list=['poasharshtestmail@gmail.com']
+        )
+    
